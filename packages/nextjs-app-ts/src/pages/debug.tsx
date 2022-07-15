c392c5ae9514f4f8bf67fdea5e00ca5b833bb46e
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-07-15 17:01:04 -0400

    created debug page

---
+++ packages/nextjs-app-ts/src/pages/debug.tsx
@@ -0,0 +1,68 @@
+import { GenericContract } from 'eth-components/ant/generic-contract';
+import { useEthersAdaptorFromProviderOrSigners } from 'eth-hooks';
+import { useEthersAppContext } from 'eth-hooks/context';
+import { useDexEthPrice } from 'eth-hooks/dapps';
+import { asEthersAdaptor } from 'eth-hooks/functions';
+import React, { FC, ReactElement } from 'react';
+
+import { useLoadAppContracts, useConnectAppContracts, useAppContracts } from '~common/components/context';
+import { useBurnerFallback } from '~common/components/hooks/useBurnerFallback';
+import { useScaffoldAppProviders } from '~common/components/hooks/useScaffoldAppProviders';
+import { MainPageFooter, MainPageHeader } from '~~/components/main';
+import {
+  BURNER_FALLBACK_ENABLED,
+  CONNECT_TO_BURNER_AUTOMATICALLY,
+  INFURA_ID,
+  LOCAL_PROVIDER,
+  MAINNET_PROVIDER,
+  TARGET_NETWORK_INFO,
+} from '~~/config/app.config';
+
+interface IDebugPageProps {
+  pageName: string;
+  children?: ReactElement;
+}
+
+const Debug: FC<IDebugPageProps> = (props) => {
+  const scaffoldAppProviders = useScaffoldAppProviders({
+    targetNetwork: TARGET_NETWORK_INFO,
+    connectToBurnerAutomatically: CONNECT_TO_BURNER_AUTOMATICALLY,
+    localProvider: LOCAL_PROVIDER,
+    mainnetProvider: MAINNET_PROVIDER,
+    infuraId: INFURA_ID,
+  });
+
+  const ethersAppContext = useEthersAppContext();
+  useBurnerFallback(scaffoldAppProviders, BURNER_FALLBACK_ENABLED);
+  useLoadAppContracts();
+  const [mainnetAdaptor] = useEthersAdaptorFromProviderOrSigners(MAINNET_PROVIDER);
+  useConnectAppContracts(mainnetAdaptor);
+  useConnectAppContracts(asEthersAdaptor(ethersAppContext));
+
+  const GLD = useAppContracts('GLD', ethersAppContext.chainId);
+  const Vendor = useAppContracts('Vendor', ethersAppContext.chainId);
+
+  const [ethPrice] = useDexEthPrice(
+    scaffoldAppProviders.mainnetAdaptor?.provider,
+    ethersAppContext.chainId !== 1 ? scaffoldAppProviders.targetNetwork : undefined
+  );
+
+  return (
+    <div className="App">
+      <MainPageHeader scaffoldAppProviders={scaffoldAppProviders} price={ethPrice} />
+      <GenericContract
+        contractName="Vendor"
+        contract={Vendor}
+        mainnetAdaptor={scaffoldAppProviders.mainnetAdaptor}
+        blockExplorer={scaffoldAppProviders.targetNetwork.blockExplorer}></GenericContract>
+      <GenericContract
+        contractName="GLD"
+        contract={GLD}
+        mainnetAdaptor={scaffoldAppProviders.mainnetAdaptor}
+        blockExplorer={scaffoldAppProviders.targetNetwork.blockExplorer}></GenericContract>
+      <MainPageFooter scaffoldAppProviders={scaffoldAppProviders} price={ethPrice} />
+    </div>
+  );
+};
+
+export default Debug;
