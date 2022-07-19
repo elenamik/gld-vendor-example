0bc014e470452dc325d8e1f39cd0e856cea4fe81
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-07-19 16:50:11 -0400

    delete stuff

--- packages/nextjs-app-ts/src/pages/index.tsx
+++ packages/nextjs-app-ts/src/pages/index.tsx
@@ -1,14 +1,5 @@
 import React, { FC, ReactElement } from 'react';
 
-/** ********************************
- * ⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️
- * See ./config/app.config.ts for configuration, such as TARGET_NETWORK
- * See ../common/src/config/appContracts.config.ts and ../common/src/config/externalContracts.config.ts to configure your contracts
- * See pageList variable below to configure your pages
- * See ../common/src/config/web3Modal.config.ts to configure the web3 modal
- * ⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️
- * ******************************** */
-
 interface IMainPageProps {
   pageName: string;
   children?: ReactElement;
